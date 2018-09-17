226e6d37614abce9f5e175401b486b6e06a20225
smorris11 <smorris11@gwu.edu>
2018-09-17 13:10:12 -0400

    Changing question1 test to test question0

--- question0.c
+++ question0.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
-int ques1(int x) {
-    int z;
-    int y  = ~x+1;
-    z= (~(y|x));
-    z = (z >> 31);
-
-    return (z & 1);
-
+int ques0(int x, int y){
+    int temp;
+    temp = ~y;
+    temp = temp +1;
+    temp = temp + x;
+    return !(temp);
 }
 
 int
